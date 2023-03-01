b63c913505e20732b7a5c8e7e0ed4d238058e39c
Richard Stephen <102833845+csr000@users.noreply.github.com>
2023-03-01 17:44:28 +0100

    connect db before app starts listening

--- src/server.ts
+++ src/server.ts
@@ -12,9 +12,6 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-// require('dotenv').config();
-// Connect to MongoDB 
-connectDB();
 
 // Express configuration
 app.set("port", process.env.PORT || 5000);
@@ -37,8 +34,11 @@ app.use("/api/enrolledcourse", enrolledcourse);
 app.use("/api/course", course);
 
 const port = app.get("port");
-const server = app.listen(port, () =>
-  console.log(`Server started on port ${port}`)
-);
 
-export default server;
+
+connectDB().then(() => {
+  app.listen(port, () =>
+    console.log(`Server started on port ${port}`)
+  );
+})
+
