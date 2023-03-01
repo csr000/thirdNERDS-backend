61c231f92a4afa4a8f334b3417dcc841dca58d71
Richard Stephen <102833845+csr000@users.noreply.github.com>
2023-03-01 17:28:31 +0100

    update with files from orignal thirdNERDS repo

---
+++ dist/utils.js
@@ -0,0 +1,18 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.writelog = exports.SERVER_ERROR = void 0;
+const http_status_codes_1 = __importDefault(require("http-status-codes"));
+const SERVER_ERROR = (res, err) => {
+    (0, exports.writelog)(err.message);
+    res.status(http_status_codes_1.default.INTERNAL_SERVER_ERROR).send("Server Error");
+};
+exports.SERVER_ERROR = SERVER_ERROR;
+const writelog = (...log) => {
+    const isProduction = true;
+    isProduction ? null : console.log(...log);
+};
+exports.writelog = writelog;
+//# sourceMappingURL=utils.js.map
--- src/server.ts
+++ src/server.ts
@@ -8,18 +8,17 @@ import course from "./routes/api/course";
 import enrolledcourse from "./routes/api/enrolledCourse";
 import cors from "cors";
 
-const app = express();
+import * as dotenv from "dotenv";
+dotenv.config();
 
-// Connect to MongoDB
+const app = express();
+// require('dotenv').config();
+// Connect to MongoDB 
 connectDB();
 
 // Express configuration
 app.set("port", process.env.PORT || 5000);
-const corsOptions = {
-  origin: '*',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb" }));
